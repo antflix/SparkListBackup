628137c5ffddaa049a69fbab9b202a7477b20b6d
Anthony <anthonymeo88@gmail.com>
2023-12-16 23:47:13 -0600

    Backup: 2023-12-16_2347

--- SparkList/components/ContactsView.swift
+++ SparkList/components/ContactsView.swift
@@ -169,8 +169,8 @@ struct ContactsView: View {
                 
             }
             if !dataManager.selectedContactName.isEmpty && !dataManager.selectedContactName2.isEmpty {
-                Text("Selected Contact 1: \(dataManager.selectedContactName)")
-                Text("Phone Number 1: \(dataManager.selectedContactPhoneNumber)").padding()
+//                Text("Selected Contact 1: \(dataManager.selectedContactName)")
+//                Text("Phone Number 1: \(dataManager.selectedContactPhoneNumber)").padding()
                 Button("Clear Contact 1") {
                     dataManager.clearFirstContact()
                 }
