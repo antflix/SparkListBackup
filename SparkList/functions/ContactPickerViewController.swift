96626a128d676b1c835e575908f440634cafd67d
Anthony <anthonymeo88@gmail.com>
2023-12-16 23:56:37 -0600

    Backup: 2023-12-16_2356

--- SparkList/functions/ContactPickerViewController.swift
+++ SparkList/functions/ContactPickerViewController.swift
@@ -43,18 +43,3 @@ struct ContactPickerViewController: UIViewControllerRepresentable {
     }
 }
 
-class Coordinator: NSObject, CNContactPickerDelegate {
-    var dataManager: DataManager
-
-    init(dataManager: DataManager) {
-        self.dataManager = dataManager
-    }
-
-    func contactPicker(_ picker: CNContactPickerViewController, didSelect contact: CNContact) {
-        if dataManager.selectedContact1 == nil {
-            dataManager.selectedContact1 = contact
-        } else {
-            dataManager.selectedContact2 = contact
-        }
-    }
-}
