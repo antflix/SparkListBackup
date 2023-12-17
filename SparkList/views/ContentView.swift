d6da39c9f23e65de1db3f32918fd564dd801274d
Anthony <anthonymeo88@gmail.com>
2023-12-16 22:10:41 -0600

    Backup: 2023-12-16_2210

--- SparkList/views/ContentView.swift
+++ SparkList/views/ContentView.swift
@@ -39,7 +39,7 @@ struct ContentView: View {
                     
                     JobsView()
                         .toolbar {
-                            ()
+                            ToolbarContent()
                         }
 //                        .navigationBarBackButtonHidden(true)// Pass
 //                        .navigationBarHidden(true)
