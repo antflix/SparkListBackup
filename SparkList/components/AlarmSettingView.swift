0a498184cc8b485bf3b4935a79457dbde980598b
Anthony <anthonymeo88@gmail.com>
2023-12-17 17:58:04 -0600

    Backup: 2023-12-17_1758

--- SparkList/components/AlarmSettingView.swift
+++ SparkList/components/AlarmSettingView.swift
@@ -4,114 +4,91 @@
 //
 //  Created by User on 12/7/23.
 //
-import SwiftUI
-import UserNotifications
-import AVFoundation
 
+import SwiftUI
 struct AlarmSettingView: View {
     @EnvironmentObject var dataManager: DataManager
 
-    @State private var selectedTime = Date()
-    @State private var isAlarmSet = false
-
+    @State private var selectedTime = Date() // State to hold the selected time
+    @State private var isAlarmSet = false // State to track if the alarm is set
+    
     var body: some View {
         VStack {
             Text("Daily Notification Schedule:")
-                .font(.title)
-                .foregroundColor(Color("Color 1"))
-            
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .foregroundStyle(Color("Color 1"))
             DatePicker("", selection: $selectedTime, displayedComponents: .hourAndMinute)
                 .datePickerStyle(WheelDatePickerStyle())
-                .foregroundColor(Color("Color 1"))
-            
-            HStack {
+                .foregroundStyle(Color("Color 1"))// Use a wheel-style picker for time selection
+            HStack{
                 Button("Set Notification ") {
-                    scheduleAlarm(at: selectedTime)
+                    scheduleAlarm(at: selectedTime, soundName: "alarm.mp3")
                     isAlarmSet = true
                 }.buttonStyle(PlainButtonStyle())
                 .padding()
                 .background(Color.green)
                 .foregroundColor(.white)
                 .cornerRadius(8)
-                
                 Button("Clear Notification") {
-                    cancelAlarm()
-                    isAlarmSet = false
-                }.buttonStyle(PlainButtonStyle())
+                        cancelAlarm() // Function to cancel the notification
+                        isAlarmSet = false
+                    }.buttonStyle(PlainButtonStyle())
                 .padding()
                 .background(Color.red)
                 .foregroundColor(.white)
                 .cornerRadius(8)
             }
-            
             if isAlarmSet {
                 Text("Alarm is set for \(formattedTime(selectedTime))")
                     .padding()
-                    .foregroundColor(Color.red)
-            } else {
+                    .foregroundStyle(Color.red)
+            }
+            else {
                 Text("Alarm is not set")
-                    .foregroundColor(Color.yellow)
+                    .foregroundStyle(Color.yellow)
             }
         }
         .padding()
     }
-
+    
+    // Function to format time for display
     private func formattedTime(_ time: Date) -> String {
         let formatter = DateFormatter()
         formatter.timeStyle = .short
         return formatter.string(from: time)
     }
-
-    func scheduleAlarm(at time: Date) {
+    func scheduleAlarm(at time: Date,  soundName: String) {
         let center = UNUserNotificationCenter.current()
         
         let content = UNMutableNotificationContent()
         content.title = "Turn In Time!!"
-        content.body = "It's time to turn in!"
-        content.sound = UNNotificationSound.default
-        
+        content.body = "Its time to turn in time!"
+        content.sound = UNNotificationSound(named: UNNotificationSoundName(rawValue: soundName))
+
         let calendar = Calendar.current
-        let dateComponents = calendar.dateComponents([.hour, .minute], from: time)
+        let dateComponents = calendar.dateComponents([.hour, .minute], from: time) // Extract hour and minute from the selected time
         
         let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
         
-        let request = UNNotificationRequest(identifier: "newAlarm1", content: content, trigger: trigger)
+        let request = UNNotificationRequest(identifier: "newAlarm", content: content, trigger: trigger)
         
         center.add(request) { error in
             if let error = error {
                 print("Error scheduling notification: \(error.localizedDescription)")
             } else {
-                print("Notification has been ran")
-                // Call function to play sound at the selected time
-                playSoundAtTime(time)
-            }
-        }
-    }
-
-    func cancelAlarm() {
-        let center = UNUserNotificationCenter.current()
-        center.removePendingNotificationRequests(withIdentifiers: ["newAlarm1"])
-    }
-
-    func playSoundAtTime(_ time: Date) {
-        let currentTime = Date()
-        let calendar = Calendar.current
-
-        // Calculate the time difference between the current time and the selected time
-        let difference = calendar.dateComponents([.second], from: currentTime, to: time).second ?? 0
-
-        // Schedule the audio playback after the time difference
-        DispatchQueue.main.asyncAfter(deadline: .now() + Double(difference)) {
-            do {
-                guard let soundURL = Bundle.main.url(forResource: "alarm", withExtension: "mp3") else {
-                    return
-                }
-
-                let audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
-                audioPlayer.play()
-            } catch let error {
-                print("Error playing sound: \(error.localizedDescription)")
+                print("Notification scheduled successfully")
             }
         }
     }
 }
+func cancelAlarm() {
+    let center = UNUserNotificationCenter.current()
+    center.removePendingNotificationRequests(withIdentifiers: ["newAlarm"]) // Replace "dailyAlarm" with your notification identifier
+}
+#if DEBUG
+struct AlarmSettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        AlarmSettingView()
+    }
+}
+#endif
