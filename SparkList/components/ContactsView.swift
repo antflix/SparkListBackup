0d8448d4cbedf34bd5ceff265929e8ee1bf28da4
Anthony <anthonymeo88@gmail.com>
2023-12-16 23:02:47 -0600

    Backup: 2023-12-16_2302

--- SparkList/components/ContactsView.swift
+++ SparkList/components/ContactsView.swift
@@ -1,5 +1,5 @@
-import SwiftUI
 import ContactsUI
+import SwiftUI
 @available(iOS 17.0, *)
 struct ContactsView: View {
     @EnvironmentObject var dataManager: DataManager // Access the DataManager
@@ -14,11 +14,11 @@ struct ContactsView: View {
                 HStack {
                     Text("Who do you turn time into?").font(Font.custom("Quicksand", size: 26).bold())
                         .frame(maxWidth: .infinity * 0.90, alignment: .center)
-                    
+
                 }.padding()
-                .background(Color.blue)
-                .foregroundColor(.white)
-                .font(.headline)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .font(.headline)
                 if !dataManager.selectedContactName.isEmpty {
                     Text("Selected Contact 1: \(dataManager.selectedContactName)")
                     Text("Phone Number 1: \(dataManager.selectedContactPhoneNumber)").padding()
@@ -31,57 +31,49 @@ struct ContactsView: View {
                     .cornerRadius(8)
                     .padding()
                 }
-                    if !dataManager.selectedContactName2.isEmpty {
-                        Text("Selected Contact 2: \(dataManager.selectedContactName2)")
-                        Text("Phone Number 2: \(dataManager.selectedContactPhoneNumber2)")
-                        Button("Clear Contact 1") {
-                                           dataManager.clearSecondContact()
-                                       }
-                                       .padding()
-                                       .foregroundColor(.white)
-                                       .background(Color.red)
-                                       .cornerRadius(8)
-                                       .padding()
-                        
-                      
-                           
-                    
-                    
-                }
-                else {
+                if !dataManager.selectedContactName2.isEmpty {
+                    Text("Selected Contact 2: \(dataManager.selectedContactName2)")
+                    Text("Phone Number 2: \(dataManager.selectedContactPhoneNumber2)")
+                    Button("Clear Contact 1") {
+                        dataManager.clearSecondContact()
+                    }
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.red)
+                    .cornerRadius(8)
+                    .padding()
+                } else {
                     Text("No Contacts Selected. Please Choose someone to send your time to.")
                         .padding()
                     Image(systemName: "person.crop.circle.badge.xmark")
                         .aspectRatio(contentMode: .fit)
-                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        .padding(/*@START_MENU_TOKEN@*/ .all/*@END_MENU_TOKEN@*/)
                         .font(Font.custom("Quicksand", size: 86).bold())
                         .symbolRenderingMode(.palette)
-                        .onAppear(){
+                        .onAppear {
                             symbolAnimation.toggle()
                         }
                         .foregroundStyle(Color.red, Color.yellow)
-                        
-                        .symbolEffect(.variableColor.reversing.cumulative, options: .repeat(100).speed(1), value:
-                                        symbolAnimation)
 
-                        
-                        
+                        .symbolEffect(.variableColor.reversing.cumulative, options: .repeat(100).speed(1), value:
+                            symbolAnimation)
                 }
-                
+
                 Spacer()
                 Button(action: {
                     self.isContact1PickerPresented = true
-                }) {HStack{
+                }) { HStack {
                     Image(systemName: "person.fill.questionmark")
                         .symbolRenderingMode(.palette)
                         .foregroundStyle(Color.red, Color.green)
 
                     Text("Select Contact 1")
-                     
-                }   .padding()
-                        .foregroundColor(.white)
-                        .background(Color.blue)
-                        .cornerRadius(8)}
+
+                }.padding()
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(8)
+                }
                 .sheet(isPresented: $isContact1PickerPresented) {
                     NavigationView {
                         ContactPickerViewController()
@@ -120,14 +112,11 @@ struct ContactsView: View {
             if !dataManager.selectedContactName.isEmpty || !dataManager.selectedContactName2.isEmpty {
                 Text("")
             }
-
- 
         }
-        .background(EllipticalGradient(colors:[Color("Color 7"), Color("Color 8")], center: .top, startRadiusFraction: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, endRadiusFraction: 0.8))
-        
+        .background(EllipticalGradient(colors: [Color("Color 7"), Color("Color 8")], center: .top, startRadiusFraction: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, endRadiusFraction: 0.8))
     }
-
 }
+
 @available(iOS 17.0, *)
 struct ContactsView_Previews: PreviewProvider {
     static var previews: some View {
