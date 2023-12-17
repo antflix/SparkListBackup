9ed74b40e4cb72069a5a265daf2cf9041d9181e9
Anthony <anthonymeo88@gmail.com>
2023-12-17 00:20:16 -0600

    Backup: 2023-12-17_0020

--- SparkList/components/ContactsView.swift
+++ SparkList/components/ContactsView.swift
@@ -95,9 +95,9 @@ struct ContactsView: View {
             if !dataManager.selectedContactName.isEmpty && dataManager.selectedContactName2.isEmpty {
 //                Text("Selected Contact 1: \(dataManager.selectedContactName)")
 //                                   Text("Phone Number 1: \(dataManager.selectedContactPhoneNumber)").padding()
-                let contact1 = dataManager.selectedContact1
-                ProfileInfoView(contact: contact1!)
-                
+                if let contact1 = dataManager.selectedContact1 {
+                    ProfileInfoView(contact: contact1)
+                }
                                    Button("Clear Contact 1") {
                                        dataManager.clearFirstContact()
                                    }
@@ -161,9 +161,9 @@ struct ContactsView: View {
                                .padding()
 //                Text("Selected Contact 2: \(dataManager.selectedContactName2)")
 //                      Text("Phone Number 2: \(dataManager.selectedContactPhoneNumber2)")
-                    let contact2 = dataManager.selectedContact2
-                    ProfileInfoView(contact: contact2!)
-                
+                if let contact2 = dataManager.selectedContact2 {
+                    ProfileInfoView(contact: contact2)
+                }
                       Button("Clear Contact 2") {
                           dataManager.clearSecondContact()
                       }
@@ -190,9 +190,9 @@ struct ContactsView: View {
                 .padding()
 //                Text("Selected Contact 2: \(dataManager.selectedContactName2)")
 //                Text("Phone Number 2: \(dataManager.selectedContactPhoneNumber2)")
-                let contact2 = dataManager.selectedContact2
-                    ProfileInfoView(contact: contact2!)
-                
+                if let contact2 = dataManager.selectedContact2 {
+                    ProfileInfoView(contact: contact2)
+                }
                 Button("Clear Contact 2") {
                     dataManager.clearSecondContact()
                 }
@@ -278,40 +278,51 @@ struct ContactsView: View {
         .background(EllipticalGradient(colors: [Color("Color 7"), Color("Color 8")], center: .top, startRadiusFraction: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, endRadiusFraction: 0.8))
     }
 }
-    struct ProfileInfoView: View {
-        var contact: CNContact // Directly using CNContact from DataManager
+struct ProfileInfoView: View {
+    var contact: CNContact // Directly using CNContact from DataManager
 
-        var body: some View {
-            VStack {
-                if let imageData = contact.imageData, let image = UIImage(data: imageData) {
-                    Image(uiImage: image)
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 100, height: 100)
-                        .clipShape(Circle())
-                        .padding()
-                }
-
-                Text(contact.givenName + " " + contact.familyName)
-                    .font(.title)
-                    .foregroundColor(.black)
-                
-                if let phoneNumber = contact.phoneNumbers.first?.value.stringValue {
-                    Text(phoneNumber)
-                        .font(.body)
-                        .foregroundColor(.gray)
-                        .padding(.bottom)
-                }
-
-                // Add more contact information as needed
-            }
-            .padding()
-            .background(Color.white)
-            .cornerRadius(10)
-            .shadow(radius: 5)
-            .padding()
+    var body: some View {
+        VStack {
+            // Card-style view to display contact information
+            CardView(contact: contact)
         }
     }
+}
+
+struct CardView: View {
+    var contact: CNContact // Directly using CNContact from DataManager
+
+    var body: some View {
+        VStack {
+            if let imageData = contact.imageData, let image = UIImage(data: imageData) {
+                Image(uiImage: image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 100, height: 100)
+                    .clipShape(Circle())
+                    .padding()
+            }
+
+            Text(contact.givenName + " " + contact.familyName)
+                .font(.title)
+                .foregroundColor(.black)
+            
+            if let phoneNumber = contact.phoneNumbers.first?.value.stringValue {
+                Text(phoneNumber)
+                    .font(.body)
+                    .foregroundColor(.gray)
+                    .padding(.bottom)
+            }
+
+            // Add more contact information as needed
+        }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(10)
+        .shadow(radius: 5)
+        .padding()
+    }
+}
 
 @available(iOS 17.0, *)
 struct ContactsView_Previews: PreviewProvider {
