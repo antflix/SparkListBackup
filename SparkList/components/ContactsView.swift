0e6d7fa5fbd6155a328b053800119770c91ae5d6
Anthony <anthonymeo88@gmail.com>
2023-12-16 23:58:03 -0600

    Backup: 2023-12-16_2358

--- SparkList/components/ContactsView.swift
+++ SparkList/components/ContactsView.swift
@@ -274,7 +274,7 @@ struct ProfileInfoView: View {
 
     var body: some View {
         VStack {
-            if let image = datacontact.photo {
+            if let image = dataManager.contact1.photo {
                 Image(uiImage: image)
                     .resizable()
                     .aspectRatio(contentMode: .fill)
