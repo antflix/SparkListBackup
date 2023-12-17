8ab2dfcf1dbe109461613e8bae8d5c5be0551dde
Anthony <anthonymeo88@gmail.com>
2023-12-16 22:42:19 -0600

    Backup: 2023-12-16_2242

--- SparkList/components/ContactsView.swift
+++ SparkList/components/ContactsView.swift
@@ -109,6 +109,9 @@ struct ContactsView: View {
  
         }
         .background(EllipticalGradient(colors:[Color("Color 7"), Color("Color 8")], center: .top, startRadiusFraction: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, endRadiusFraction: 0.8))
+        func resetEmployeeData() {
+                dataManager.employeeData = [:] // Resetting employeeData to an empty dictionary
+            }
     }
 }
 @available(iOS 17.0, *)
