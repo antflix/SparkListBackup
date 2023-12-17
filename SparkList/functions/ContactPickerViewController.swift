9610d77905e5f33fe268ea8ecf5af253bcaf2d24
Anthony <anthonymeo88@gmail.com>
2023-12-16 23:53:30 -0600

    Backup: 2023-12-16_2353

--- SparkList/functions/ContactPickerViewController.swift
+++ SparkList/functions/ContactPickerViewController.swift
@@ -1,13 +1,41 @@
-func contactPicker(_ picker: CNContactPickerViewController, didSelect contact: CNContact) {
-       if dataManager.selectedContact1 == nil {
-           dataManager.selectedContact1 = contact
-       } else {
-           dataManager.selectedContact2 = contact
-       }
-   } func contactPicker(_ picker: CNContactPickerViewController, didSelect contact: CNContact) {
-       if dataManager.selectedContact1 == nil {
-           dataManager.selectedContact1 = contact
-       } else {
-           dataManager.selectedContact2 = contact
-       }
-   }
+import SwiftUI
+import ContactsUI
+
+struct ContactPickerViewController: UIViewControllerRepresentable {
+    @EnvironmentObject var dataManager: DataManager
+
+    func makeUIViewController(context: Context) -> CNContactPickerViewController {
+        let picker = CNContactPickerViewController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: CNContactPickerViewController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(dataManager: dataManager)
+    }
+
+    class Coordinator: NSObject, CNContactPickerDelegate {
+        var dataManager: DataManager
+
+        init(dataManager: DataManager) {
+            self.dataManager = dataManager
+        }
+
+        func contactPicker(_ picker: CNContactPickerViewController, didSelect contact: CNContact) {
+            let fullName = CNContactFormatter.string(from: contact, style: .fullName) ?? ""
+            let phoneNumbers = contact.phoneNumbers.first?.value.stringValue ?? ""
+
+            // Update DataManager directly when a contact is selected
+            if dataManager.selectedContactName.isEmpty {
+                dataManager.selectedContactName = fullName
+                dataManager.selectedContactPhoneNumber = phoneNumbers
+            } else {
+                dataManager.selectedContactName2 = fullName
+                dataManager.selectedContactPhoneNumber2 = phoneNumbers
+            }
+        }
+    }
+}
+
