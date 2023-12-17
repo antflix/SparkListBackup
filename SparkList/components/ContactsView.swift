25d6a861ee9d82aba878b991b39c4ddb831748ff
Anthony <anthonymeo88@gmail.com>
2023-12-16 18:17:30 -0600

    Backup: 2023-12-16_1817

--- SparkList/components/ContactsView.swift
+++ SparkList/components/ContactsView.swift
@@ -4,7 +4,6 @@ struct ContactsView: View {
     @EnvironmentObject var dataManager: DataManager // Access the DataManager
 
     @State private var isContactPickerPresented = false
-    @State private var selectedContactName = ""
     @State private var selectedContactPhoneNumber = ""
     @State private var selectedContactName2 = ""
     @State private var selectedContactPhoneNumber2 = ""
@@ -15,16 +14,16 @@ struct ContactsView: View {
                 self.isContactPickerPresented = true
             }
             .sheet(isPresented: $isContactPickerPresented) {
-                ContactPickerViewController(selectedContactName: $selectedContactName, selectedContactPhoneNumber: $selectedContactPhoneNumber, selectedContactName2: $selectedContactName2, selectedContactPhoneNumber2: $selectedContactPhoneNumber2) // Update to include additional bindings
+                ContactPickerViewController(selectedContactName: $dataManager.selectedContactName, selectedContactPhoneNumber: $dataManager.selectedContactPhoneNumber, selectedContactName2: $dataManager.selectedContactName2, selectedContactPhoneNumber2: $dataManager.selectedContactPhoneNumber2) // Update to include additional bindings
             }
 
-            if !selectedContactName.isEmpty && !selectedContactPhoneNumber.isEmpty {
-                Text("Selected Contact 1: \(selectedContactName)")
-                Text("Phone Number 1: \(selectedContactPhoneNumber)")
+            if !dataManager.selectedContactName.isEmpty && !dataManager.selectedContactPhoneNumber.isEmpty {
+                Text("Selected Contact 1: \(dataManager.selectedContactName)")
+                Text("Phone Number 1: \(dataManager.selectedContactPhoneNumber)")
             }
-            if !selectedContactName2.isEmpty && !selectedContactPhoneNumber2.isEmpty {
-                Text("Selected Contact 2: \(selectedContactName2)")
-                Text("Phone Number 2: \(selectedContactPhoneNumber2)")
+            if !dataManager.selectedContactName2.isEmpty && !dataManager.selectedContactPhoneNumber2.isEmpty {
+                Text("Selected Contact 2: \(dataManager.selectedContactName2)")
+                Text("Phone Number 2: \(dataManager.selectedContactPhoneNumber2)")
             }
         }
     }
