2986aba7e0c335cd821bc657185ae30a2c4003e2
Anthony <anthonymeo88@gmail.com>
2023-12-16 18:33:10 -0600

    Backup: 2023-12-16_1833

--- SparkList/components/ContactsView.swift
+++ SparkList/components/ContactsView.swift
@@ -1,30 +1,47 @@
-// ContactPickerView.swift
 import SwiftUI
+import ContactsUI
 struct ContactsView: View {
     @EnvironmentObject var dataManager: DataManager // Access the DataManager
 
-    @State private var isContactPickerPresented = false
-    @State private var selectedContactPhoneNumber = ""
-    @State private var selectedContactName2 = ""
-    @State private var selectedContactPhoneNumber2 = ""
+    @State private var isContact1PickerPresented = false
+    @State private var isContact2PickerPresented = false
 
     var body: some View {
         VStack {
-            Button("Pick Contact") {
-                self.isContactPickerPresented = true
-            }
-            .sheet(isPresented: $isContactPickerPresented) {
-                ContactPickerViewController() // Update to include additional bindings
+            if dataManager.selectedContactName.isEmpty || dataManager.selectedContactName2.isEmpty {
+                Button(action: {
+                    self.isContact1PickerPresented = true
+                }) {
+                    Text("Select Contact 1")
+                }
+                .sheet(isPresented: $isContact1PickerPresented) {
+                    ContactPickerViewController()
+                        .environmentObject(dataManager)
+                }
             }
 
-            if !dataManager.selectedContactName.isEmpty && !dataManager.selectedContactPhoneNumber.isEmpty {
+            if !dataManager.selectedContactName.isEmpty {
                 Text("Selected Contact 1: \(dataManager.selectedContactName)")
                 Text("Phone Number 1: \(dataManager.selectedContactPhoneNumber)")
+
+                if dataManager.selectedContactName2.isEmpty {
+                    Button(action: {
+                        self.isContact2PickerPresented = true
+                    }) {
+                        Text("Select Contact 2")
+                    }
+                    .sheet(isPresented: $isContact2PickerPresented) {
+                        ContactPickerViewController()
+                            .environmentObject(dataManager)
+                    }
+                }
             }
-            if !dataManager.selectedContactName2.isEmpty && !dataManager.selectedContactPhoneNumber2.isEmpty {
+
+            if !dataManager.selectedContactName2.isEmpty {
                 Text("Selected Contact 2: \(dataManager.selectedContactName2)")
                 Text("Phone Number 2: \(dataManager.selectedContactPhoneNumber2)")
             }
         }
+        .padding()
     }
 }
