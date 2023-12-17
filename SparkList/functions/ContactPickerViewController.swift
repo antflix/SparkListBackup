00e5ea6d17ab94b169d27e68dc76363d2684ee24
Anthony <anthonymeo88@gmail.com>
2023-12-16 18:25:05 -0600

    Backup: 2023-12-16_1825

--- SparkList/functions/ContactPickerViewController.swift
+++ SparkList/functions/ContactPickerViewController.swift
@@ -2,39 +2,41 @@ import SwiftUI
 import ContactsUI
 
 struct ContactPickerViewController: UIViewControllerRepresentable {
-    @EnvironmentObject var dataManager: DataManager // Access the DataManager
-
+    @EnvironmentObject var dataManager: DataManager
     @Binding var selectedContactName: String
     @Binding var selectedContactPhoneNumber: String
     @Binding var selectedContactName2: String // Additional binding for second contact name
     @Binding var selectedContactPhoneNumber2: String // Additional binding for second contact phone number
 
- 
-    func makeUIViewController(context: Context) -> CNContactPickerViewController {
-           let picker = CNContactPickerViewController()
-           picker.delegate = context.coordinator
+    func makeUIViewController(context: Context) -> UIViewController {
+          let picker = CNContactPickerViewController()
+          picker.delegate = context.coordinator
 
-           // Modify the appearance of the contact picker
-           picker.navigationBar.tintColor = UIColor.systemBlue // Change navigation bar tint color
-           picker.view.backgroundColor = UIColor.white // Set background color
+          let navigationController = UINavigationController(rootViewController: picker)
+          navigationController.navigationBar.tintColor = UIColor.systemBlue
+          navigationController.view.backgroundColor = UIColor.white
 
-           return picker
-       }
+          return navigationController
+      }
 
-    func updateUIViewController(_ uiViewController: CNContactPickerViewController, context: Context) {}
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+          // Implement update logic if needed
+      }
+
+
 
     func makeCoordinator() -> Coordinator {
-         Coordinator(parent: self, dataManager: dataManager)
-     }
+        Coordinator(dataManager: dataManager)
+    }
 
-     class Coordinator: NSObject, CNContactPickerDelegate {
-         let parent: ContactPickerViewController
-         var dataManager: DataManager
 
-         init(parent: ContactPickerViewController, dataManager: DataManager) {
-             self.parent = parent
-             self.dataManager = dataManager
-         }
+    class Coordinator: NSObject, CNContactPickerDelegate {
+        var dataManager: DataManager
+
+        init(dataManager: DataManager) {
+            self.dataManager = dataManager
+        }
 
            func contactPicker(_ picker: CNContactPickerViewController, didSelect contact: CNContact) {
                let fullName = CNContactFormatter.string(from: contact, style: .fullName) ?? ""
