b6d5d4187691daad377d704c02f0046b37d8bae0
Anthony <anthonymeo88@gmail.com>
2023-12-18 03:33:37 -0600

    Backup: 2023-12-18_0333

--- SparkList/views/EmployeeView.swift
+++ SparkList/views/EmployeeView.swift
@@ -202,6 +202,12 @@ struct EmployeeView: View {
             .navigationBarBackButtonHidden(true) // Hides the back button
             .navigationBarHidden(true)
         
+    }.onChange(of: dataManager.isDarkMode) { newValue in
+        UserDefaults.standard.set(newValue, forKey: "isDarkMode")
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let window = windowScene.windows.first {
+            window.rootViewController?.overrideUserInterfaceStyle = newValue ? .dark : .light
+        }
     }
         
     //        .foregroundColor(colorScheme == .dark ? Color.blue : Color.black)
