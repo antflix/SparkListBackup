cc9675bcd14699c994968af43e6cead3304fbb72
Anthony <anthonymeo88@gmail.com>
2023-12-20 22:34:32 -0600

    Backup: 2023-12-20_2234

--- SparkList/functions/example.swift
+++ SparkList/functions/example.swift
@@ -5,7 +5,7 @@ struct ContactsSelectionView: View {
     @EnvironmentObject var dataManager: DataManager
     @State private var selectedContacts: [CNContact]? = []
     @State private var isContact1PickerPresented = false
-
+    
     
     var body: some View {
         VStack {
@@ -29,41 +29,68 @@ struct ContactsSelectionView: View {
                 
                 
             }
-
+            
             if let contacts = selectedContacts, !contacts.isEmpty {
-                         List {
-                             ForEach(contacts, id: \.self) { contact in
-                                 ContactRow(contact: contact) {
-                                     if let index = selectedContacts?.firstIndex(of: contact) {
-                                         selectedContacts?.remove(at: index)
-                                         dataManager.saveSelectedContacts(contacts)
-                                     }
-                                 }
-                             }
-                         }
-                     } else {
-                         Text("No contacts selected")
-                     }
-                 }
-                 .onAppear {
-                     // Retrieve saved contacts from UserDefaults
-                     if let savedContacts = dataManager.retrieveSelectedContacts() {
-                         self.selectedContacts = savedContacts
-                     }
-                 }
-             }
-         }
+                List {
+                    ForEach(contacts, id: \.self) { contact in
+                        ContactRow(contact: contact) {
+                            if let index = selectedContacts?.firstIndex(of: contact) {
+                                selectedContacts?.remove(at: index)
+                                dataManager.saveSelectedContacts(contacts)
+                            }
+                        }
+                    }
+                }
+            } else {
+                Text("No contacts selected")
+            }
+            if let contacts = selectedContacts, !contacts.isEmpty {
+                List {
+                    ForEach(contacts, id: \.self) { contact in
+                        ContactRow(contact: contact) {
+                            // Pass the contact to be deleted to the onDelete action
+                            deleteContact(contact)
+                        }
+                    }
+                }
+            } else {
+                Text("No contacts selected")
+            }
+        }
+        // ... existing code for retrieving saved contacts
+        
+        
+        
+        .onAppear {
+            // Retrieve saved contacts from UserDefaults
+            if let savedContacts = dataManager.retrieveSelectedContacts() {
+                self.selectedContacts = savedContacts
+            }
+        }
+    }
+        func deleteContact(_ contact: CNContact) {
+            if let index = selectedContacts?.firstIndex(of: contact) {
+                selectedContacts?.remove(at: index)
+                dataManager.saveSelectedContacts(selectedContacts ?? [])
+            }
+        }
+    }
+
 struct ContactRow: View {
     let contact: CNContact
-    let onDelete: () -> Void
-
+    let onDelete: () -> Void // No need for onDelete in this context
+    
     var body: some View {
         HStack {
             Text("\(contact.givenName) \(contact.familyName)")
             Spacer()
-            Button(action: dataManager.deleteSelectedContacts) {
+            Button(action: onDelete) {
                 Image(systemName: "trash")
                     .foregroundColor(.red)
+                    .onTapGesture {
+                        // Perform the delete action for this specific contact
+                        onDelete()
+                    }
             }
         }
     }
