0fc3a69874ddf9eef59ddbdfce7c237619b2ae7b
Anthony <anthonymeo88@gmail.com>
2023-12-20 22:08:03 -0600

    Saving contacts and extracting phone number working

--- SparkList/views/PreViews.swift
+++ SparkList/views/PreViews.swift
@@ -8,7 +8,7 @@
 import Foundation
 import MessageUI
 import SwiftUI
-import ContactsUI
+
 @available(iOS 17.0, *)
 struct PreViews: View {
     @State private var messageText = "Content from the preview app"
@@ -17,16 +17,13 @@ struct PreViews: View {
     @State private var customPhoneNumber: String = UserDefaults.standard.string(forKey: "CustomPhoneNumber") ?? ""
     @State private var customPhoneNumber2: String = UserDefaults.standard.string(forKey: "CustomPhoneNumber2") ?? ""
     @State private var showAlert = false
-    @State private var selectedContacts: [CNContact]? = []
-
     @State private var settingsPopover = false // Create a state variable to control popover visibility//    @Binding var isSettingsViewPresented: Bool
 //    @State private var recipient: String = UserDefaults.standard.string(forKey: "CustomPhoneNumber") ?? ""
     // To save the formatted data for later use:
     @State private var savedData: String = "" // State variable to store the formatted data
-    @State private var phoneNumbersString: String = ""
+    
     
     func generateSMSBody() {
-        
         let sortedOutput = SMSGenerator.sortedFormat(dataManager: dataManager)
         let smsBodyWithDate = SMSGenerator.generateSMSURL(
             sortedOutput: sortedOutput, dataManger: dataManager)
@@ -52,169 +49,163 @@ struct PreViews: View {
                dataManager.selectedPhoneNumber = "\(dataManager.selectedContactPhoneNumber)"
            }
        }
-    
-    
-  
-    var body: some View {
-        VStack {
-            //        let sortedOutput = SMSGenerator.sortedFormat(dataManager: dataManager)
-            //        let smsBodyWithDate = SMSGenerator.generateSMSURL(
-            //
-            
-            
-            
-            //    sortedOutput: sortedOutput, dataManger: dataManager)
-            
-            
-         
-            
-            let phoneNumbersString = dataManager.selectedContacts?.compactMap { contact -> String? in
-                guard let firstPhoneNumber = contact.phoneNumbers.first?.value.stringValue else {
-                    return nil // Skip contacts without phone numbers
-                }
-                return firstPhoneNumber
-            }.joined(separator: ", ")
-            
-            
-            
-            let smsURLString = "sms:/open?addresses=\(phoneNumbersString ?? "")&body=\(dataManager.allSMSs)"
-            //      let deviceBg = #colorLiteral(red: 0, green: 0.3725490196, blue: 1, alpha: 1)
-            VStack {
-                HStack {
-                    Text("Text Preview").font(Font.custom("Quicksand", size: 30).bold())
-                        .frame(maxWidth: .infinity * 0.90, alignment: .leading)
-                    Button(action: {
-                        // Toggle the popover state for this particular row
-                        showingPopover.toggle()
-                    }) {
-                        Image(systemName: "questionmark.circle.fill")
-                            .foregroundColor(.white)
-                    }
-                    .buttonStyle(PlainButtonStyle()) //
-                    .popover(isPresented: $showingPopover) {
-                        VStack {
-                            // Define your popover content here
-                            Text("Instructions:")
-                                .foregroundColor(Color("Color 6"))
-                                .font(.title)
-                            Divider()
-                                .padding(.horizontal)
-                            
-                            Text("1) PreView.swift placeholder").foregroundColor(Color("Color 6"))
-                            
-                        }.padding()
-                    }
-                }
-                
-                .padding()
-                .background(Color.blue)
-                .foregroundColor(.white)
-                .font(.headline)
-                
-                VStack {
-                    darkmode()
-                    HStack {
-                        Text("TIME!!!")
-                            .padding()
-                            .foregroundColor(.primary)
-                            .background(Color.green.opacity(0.9))
-                            .clipShape(BubbleShape(myMessage: false))
-                            .font(.system(size: 14.0))
-                        
-                        Spacer()
-                    }.padding(.trailing, 55).padding(.vertical, 10)
-                    
-                    HStack {
-                        Spacer()
-                        Text("\(dataManager.allSMSs)")
-                            .padding()
-                            .background(Color(UIColor.systemBlue))
-                            .clipShape(BubbleShape(myMessage: true))
-                            .foregroundColor(.white)
-                            .multilineTextAlignment(.leading) // Aligning the text to the right
-                            .font(.system(size: 14.0))
-                    }.padding(.leading, 55).padding(.vertical, 10)
-                    Spacer()
-                    Divider().frame(height: 1.0).background(
-                        Color("Color 6")
-                    ).padding()
-                }.padding(.horizontal, 30)
-                    .clipShape(RoundedRectangle(cornerRadius: 20)) // Adding rounded corners
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 20) // Overlay for border
-                            .stroke(Color("Color 6"), lineWidth: 1)
-                            .padding(.horizontal) // Border color and width
-                    )
-                Spacer()
-                VStack {
-                    Button(
-                        action: { sendMessage(sms: smsURLString) },
-                        label: {
-                            Text("Send Message")
-                                .font(.title)
-                                .foregroundColor(Color.green)
-                                .background(Color.clear)
-                            Image(systemName: "arrow.up.circle.fill")
-                                .background(Color.clear)
-                                .foregroundStyle(Color.green)
-                                .font(.title)
-                        }
-                        
-                    )
-                    .buttonStyle(PlainButtonStyle()) //
-                    .padding()
-                    NavigationLink(destination: JobsView().navigationBarHidden(true)) {
-                        HStack {
-                            Text("Add More Time")
-                                .foregroundColor(Color.orange)
-                                .background(Color.clear)
-                            
-                            Image(systemName: "hourglass.badge.plus")
-                                .foregroundColor(Color.orange)
-                                .font(.title)
-                                .background(Color.clear)
-                        }
-                    }
-                    .ignoresSafeArea()
-                    .buttonStyle(PlainButtonStyle())
-                    .navigationBarBackButtonHidden(true)
-                    .navigationBarHidden(true)
-                    .onAppear {
-                        generateSMSBody()
-                    }
-                }
-            }.toolbar{MyToolbarItems()}
-                .background(EllipticalGradient(colors:[Color("Color 7"), Color("Color 8")], center: .top, startRadiusFraction: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, endRadiusFraction: 0.8))
-                .onChange(of: dataManager.isDarkMode) { newValue in
-                    UserDefaults.standard.set(newValue, forKey: "isDarkMode")
-                    if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-                       let window = windowScene.windows.first {
-                        window.rootViewController?.overrideUserInterfaceStyle = newValue ? .dark : .light
-                    }
-                }
-            
-            //            .alert(isPresented: $showAlert) {
-            //                Alert(
-            //                    title: Text("Enter Phone Number"),
-            //                    message: Text("Please enter a phone number"),
-            //                    Button: .default(
-            //                        Text("Add Phone Number"),
-            //                        action: {
-            //                            // Toggle popover state
-            //                            settingsPopover.toggle()
-            //                        }),
-            //                    Button: .cancel(Text("Cancel"))
-            //
-            //                )
-            
-            
-            //                            dismissButton: .default(Text("OK"))
-        }.onAppear {
-            // Retrieve saved contacts from UserDefaults
+    func retrieveAndFormatContacts() -> String {
+            // Retrieve saved contacts from UserDefaults using your DataManager
             if let savedContacts = dataManager.retrieveSelectedContacts() {
-                self.selectedContacts = savedContacts
+                // Extract the first phone number of each contact and join them with a comma
+                let phoneNumbersString = savedContacts.compactMap { contact -> String? in
+                    guard let firstPhoneNumber = contact.phoneNumbers.first?.value.stringValue else {
+                        return nil // Skip contacts without phone numbers
+                    }
+                    return firstPhoneNumber
+                }.joined(separator: ", ")
+
+                return phoneNumbersString // Return the formatted string
+            }
+            return "" // Return an empty string if there are no saved contacts
+        }
+
+    var body: some View {
+//        let sortedOutput = SMSGenerator.sortedFormat(dataManager: dataManager)
+//        let smsBodyWithDate = SMSGenerator.generateSMSURL(
+//            sortedOutput: sortedOutput, dataManger: dataManager)
+        
+        
+    
+        let smsURLString = "sms:/open?addresses=\(retrieveAndFormatContacts())&body=\(dataManager.allSMSs)"
+
+        //      let deviceBg = #colorLiteral(red: 0, green: 0.3725490196, blue: 1, alpha: 1)
+        return VStack {
+            HStack {
+                Text("Text Preview").font(Font.custom("Quicksand", size: 30).bold())
+                    .frame(maxWidth: .infinity * 0.90, alignment: .leading)
+                Button(action: {
+                    // Toggle the popover state for this particular row
+                    showingPopover.toggle()
+                }) {
+                    Image(systemName: "questionmark.circle.fill")
+                        .foregroundColor(.white)
+                }
+                .buttonStyle(PlainButtonStyle()) //
+                .popover(isPresented: $showingPopover) {
+                    VStack {
+                        // Define your popover content here
+                        Text("Instructions:")
+                            .foregroundColor(Color("Color 6"))
+                            .font(.title)
+                        Divider()
+                            .padding(.horizontal)
+                        
+                        Text("1) PreView.swift placeholder").foregroundColor(Color("Color 6"))
+                        
+                    }.padding()
+                }
+            }
+            
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .font(.headline)
+            
+            VStack {
+                darkmode()
+                HStack {
+                    Text("TIME!!!")
+                        .padding()
+                        .foregroundColor(.primary)
+                        .background(Color.green.opacity(0.9))
+                        .clipShape(BubbleShape(myMessage: false))
+                        .font(.system(size: 14.0))
+                    
+                    Spacer()
+                }.padding(.trailing, 55).padding(.vertical, 10)
+                
+                HStack {
+                    Spacer()
+                    Text("\(dataManager.allSMSs)")
+                        .padding()
+                        .background(Color(UIColor.systemBlue))
+                        .clipShape(BubbleShape(myMessage: true))
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.leading) // Aligning the text to the right
+                        .font(.system(size: 14.0))
+                }.padding(.leading, 55).padding(.vertical, 10)
+                Spacer()
+                Divider().frame(height: 1.0).background(
+                    Color("Color 6")
+                ).padding()
+            }.padding(.horizontal, 30)
+                .clipShape(RoundedRectangle(cornerRadius: 20)) // Adding rounded corners
+                .overlay(
+                    RoundedRectangle(cornerRadius: 20) // Overlay for border
+                        .stroke(Color("Color 6"), lineWidth: 1)
+                        .padding(.horizontal) // Border color and width
+                )
+            Spacer()
+            VStack {
+                Button(
+                    action: { sendMessage(sms: smsURLString) },
+                    label: {
+                        Text("Send Message")
+                            .font(.title)
+                            .foregroundColor(Color.green)
+                            .background(Color.clear)
+                        Image(systemName: "arrow.up.circle.fill")
+                            .background(Color.clear)
+                            .foregroundStyle(Color.green)
+                            .font(.title)
+                    }
+                    
+                )
+                .buttonStyle(PlainButtonStyle()) //
+                    .padding()
+                NavigationLink(destination: JobsView().navigationBarHidden(true)) {
+                    HStack {
+                        Text("Add More Time")
+                            .foregroundColor(Color.orange)
+                            .background(Color.clear)
+                        
+                        Image(systemName: "hourglass.badge.plus")
+                            .foregroundColor(Color.orange)
+                            .font(.title)
+                            .background(Color.clear)
+                    }
+                }
+                .ignoresSafeArea()
+                .buttonStyle(PlainButtonStyle())
+                .navigationBarBackButtonHidden(true)
+                .navigationBarHidden(true)
+                .onAppear {
+                    generateSMSBody()
+                }
+            }
+        }.toolbar{MyToolbarItems()}
+        .background(EllipticalGradient(colors:[Color("Color 7"), Color("Color 8")], center: .top, startRadiusFraction: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, endRadiusFraction: 0.8))
+        .onChange(of: dataManager.isDarkMode) { newValue in
+            UserDefaults.standard.set(newValue, forKey: "isDarkMode")
+            if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+               let window = windowScene.windows.first {
+                window.rootViewController?.overrideUserInterfaceStyle = newValue ? .dark : .light
             }
         }
+        
+        //            .alert(isPresented: $showAlert) {
+        //                Alert(
+        //                    title: Text("Enter Phone Number"),
+        //                    message: Text("Please enter a phone number"),
+        //                    Button: .default(
+        //                        Text("Add Phone Number"),
+        //                        action: {
+        //                            // Toggle popover state
+        //                            settingsPopover.toggle()
+        //                        }),
+        //                    Button: .cancel(Text("Cancel"))
+        //
+        //                )
+        
+        
+        //                            dismissButton: .default(Text("OK"))
+        
     }
     
 }
